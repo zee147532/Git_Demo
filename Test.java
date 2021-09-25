b998fce242d971ac541575b7f0eb4e1e65ced1fe
ND Tien <zeezxt@gmail.com>
2021-09-25 16:39:20 +0700

    Lai sua file Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test!");
         System.out.println("Them mot dong");
+        System.out.println("Lai them mot dong");
     }
 }
